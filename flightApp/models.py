45612022da5c75578a367986bf8b91d594ac723c
Jang-Jaewon <jewon119@gmail.com>
2022-02-16 12:06:55 +0900

    Add: Create Model and Serializer in flightApp

--- flightApp/models.py
+++ flightApp/models.py
@@ -1,3 +1,25 @@
+from tkinter import CASCADE
 from django.db import models
 
-# Create your models here.
+from core.models import TimeStampModel
+
+
+class Flight(TimeStampModel):
+    flightNumber             = models.CharField(max_length=10)
+    operatingAirline         = models.CharField(max_length=20)
+    departureCity            = models.CharField(max_length=20)
+    arriveCity               = models.CharField(max_length=20)
+    dateOfDeparture          = models.DateField()
+    estimatedTimeOfDeparture = models.DateTimeField()
+
+
+class Passenger(TimeStampModel):
+    firstName = models.CharField(max_length=20)
+    lastName  = models.CharField(max_length=20)
+    email     = models.CharField(max_length=20)
+    phone     = models.CharField(max_length=20)
+
+
+class Reservation(TimeStampModel):
+    flight    = models.ForeignKey('Flight', on_delete=models.CASCADE)
+    passenger = models.OneToOneField('Passenger', on_delete=models.CASCADE)
