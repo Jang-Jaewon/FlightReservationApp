b8479f82cc03d26a165c651b0184600cab223e8c
Jang-Jaewon <jewon119@gmail.com>
2022-02-17 12:23:51 +0900

    Modify: Serializer adds validation using regular expressions.

--- flightApp/serializers.py
+++ flightApp/serializers.py
@@ -1,3 +1,5 @@
+import re
+
 from rest_framework import serializers
 
 from flightApp.models import Flight, Passenger, Reservation
@@ -7,6 +9,11 @@ class FlightSerializer(serializers.ModelSerializer):
     class Meta:
         model = Flight
         fields = '__all__'
+
+    def validate_flightNumber(self, flightNumber):
+        if(re.match("a-zA-Z0-9*$", flightNumber) == None):
+            raise serializers.ValidationError("Invalid Flight Number. Make sure it is alpha number.")
+        return flightNumber
 
     
 class PassengerSerializer(serializers.ModelSerializer):
